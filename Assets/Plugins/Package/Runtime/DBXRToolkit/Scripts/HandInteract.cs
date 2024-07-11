5b912453d2fc9c0deafc055f2f2ea3f255516ce6
jdemartini801 <98787297+jdemartini801@users.noreply.github.com>
2024-07-11 09:25:35 -0400

    Latest

--- Assets/Plugins/Package/Runtime/DBXRToolkit/Scripts/HandInteract.cs
+++ Assets/Plugins/Package/Runtime/DBXRToolkit/Scripts/HandInteract.cs
@@ -7,11 +7,19 @@ public class HandInteract : MonoBehaviour
     // The hand transform
     protected Transform handTransform;
     protected Velocity velComponent;
+    private PointerInteract pointer;
 
     protected void Awake()
     {
         velComponent = transform.parent.GetComponentInChildren<Velocity>();
+        pointer = transform.parent.parent.GetComponentInChildren<PointerInteract>();
     }
+
+    public PointerInteract GetPointer()
+    {
+        return pointer;
+    }
+
     public Vector3 GetVelocity()
     {
         return velComponent.GetVelocity();
